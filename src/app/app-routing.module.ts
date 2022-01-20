0d734a062711e798070a5377a25760d387a867dd
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-01-20 11:31:13 -0300

    comeco do crud para categ

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriaReadComponent } from './components/view/categoria/categoria-read/categoria-read.component';
 import { HomeComponent } from './components/view/home/home.component';
 
 const routes: Routes = [
-  {path:"", component: HomeComponent}
+  {path:'', component: HomeComponent},
+  {path:'categorias', component:CategoriaReadComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,9 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './components/view/home/home.component';
 import {MatCardModule} from '@angular/material/card';
+import { CategoriaReadComponent } from './components/view/categoria/categoria-read/categoria-read.component';
+import {MatTableModule} from '@angular/material/table';
+
 
 
 @NgModule({
@@ -22,7 +25,8 @@ import {MatCardModule} from '@angular/material/card';
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    CategoriaReadComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,7 +36,8 @@ import {MatCardModule} from '@angular/material/card';
     MatSidenavModule,
     MatListModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
