8989dab083eba6f2f8afc33c48c0ad854f41d3cf
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-01-24 10:48:41 -0300

    metodo create para livros

--- src/app/components/view/livro/livro-create/livro-create.component.ts
+++ src/app/components/view/livro/livro-create/livro-create.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Livro } from '../livro.model';
+import { LivroService } from '../livro.service';
 
 @Component({
   selector: 'app-livro-create',
@@ -12,9 +15,28 @@ export class LivroCreateComponent implements OnInit {
   nome_autor=new FormControl('', Validators.minLength(10));
   texto=new FormControl('', Validators.minLength(10));
 
-  constructor() { }
+  constructor(private service: LivroService, private route: ActivatedRoute, private router: Router) { }
+
+id_cat: String="";
+livro: Livro={
+  id:'',
+  titulo:'',
+  nome_autor:'',
+  texto:''
+}
 
   ngOnInit(): void {
+    this.id_cat=this.route.snapshot.paramMap.get('id_cat')!
+  }
+
+  create(): void{
+    this.service.create(this.livro, this.id_cat).subscribe((resposta)=>{
+      this.router.navigate(["categorias/"+this.id_cat+"/livros"]);
+      this.service.mensagem("Livro adicionado com sucesso!");
+      }, err => {
+        this.router.navigate(["categorias/"+this.id_cat+"/livros"]);
+        this.service.mensagem("Erro na adição de novo livro, tente mais tarde!");
+      });
   }
 
   getMessage(){
--- src/app/components/view/livro/livro.service.ts
+++ src/app/components/view/livro/livro.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Livro } from './livro.model';
@@ -11,12 +12,23 @@ export class LivroService {
 
   baseUrl: String = environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private snack: MatSnackBar) { }
 
   findAllByCategoria(id_cat: String): Observable<Livro[]>{
     const url = this.baseUrl + "/livros?categoria=" + id_cat;
     return this.http.get<Livro[]>(url);
   }
+  create(livro: Livro, id_cat: String): Observable<Livro>{
+    const url = this.baseUrl+"/livros?categoria="+id_cat;
+    return this.http.post<Livro>(url, livro);
+  }
 
+  mensagem(str: string): void{
+    this.snack.open(`${str}`, 'OK', {
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      duration: 3000
+    })
+}
   
 }
